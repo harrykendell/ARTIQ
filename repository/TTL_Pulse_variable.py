b28970b595077e094255335f6837e1a4cb672846
Harry Kendell <harry@kendell.co.uk>
2024-05-01 18:41:04 +0100

    keep going until enter is pressed

--- repository/TTL_Pulse_variable.py
+++ repository/TTL_Pulse_variable.py
@@ -1,11 +1,13 @@
 from artiq.experiment import *
+from utils.surpress_missing_imports import *
+from utils.wait_for_enter import is_enter_pressed
 
 # turns output on, off, and then pulses it
 # to view the trace from this on a scope, use a single trigger with at least 16ms measured on scope
 
 
-class TTL_Output_On_Off_Pulse(EnvExperiment):
-    """TTL Output On, Off, Pulse"""
+class TTL_Pulse_Train(EnvExperiment):
+    """TTL pulse train - variable port"""
 
     # This code runs on the host device
     def build(self):
@@ -23,15 +25,17 @@ class TTL_Output_On_Off_Pulse(EnvExperiment):
     def run(self):
 
         self.core.reset()  # resets core device
-        self.ttl.output()  # sets TTL as an output
-        # moves timestamp forward to prevent collision between ttl.output and ttl.on although appears not to be neccessary in this case.
-        delay(1 * us)
+        self.ttl.output()
 
-        # sets TTL output high for 5ms then sets it to low
-        self.ttl.pulse(5 * ms)
-        delay(1 * ms)
-
-        # leave it high for a second
-        self.ttl.on()
-        delay(1*s)
-        self.ttl.off()
+        # we have to delay to allow is_enter_pressed to check for keys
+        # delay(0.25*s)
+        print("Press ENTER to cancel.")
+        while not is_enter_pressed():
+            self.core.break_realtime()
+            # do not fill the FIFOs too much to avoid long response times
+            t = now_mu() - self.core.seconds_to_mu(0.2)
+            while self.core.get_rtio_counter_mu() < t:
+                pass
+            for _ in range(100_000):
+                delay(2 * us)
+                self.ttl.pulse(2 * us)
