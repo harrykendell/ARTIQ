41d701dec7cde7e099b6d88f1ceff55295dd71e5
Harry Kendell <harry@kendell.co.uk>
2024-05-02 15:09:28 +0100

    unneeded import

--- repository/Utils/surpress_missing_imports.py
+++ repository/Utils/surpress_missing_imports.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-from artiq.experiment import *
 from artiq.language.core import delay, now_mu, parallel, sequential
 from artiq.language.units import us, ms, MHz, dB, s
 from artiq.language.types import TInt32, TFloat, TList, TTuple, TStr, TNone
