4c41af47e6305c7f697592fbea17bdbd9321e355
Harry Kendell <harry@kendell.co.uk>
2024-05-03 12:05:54 +0100

    it looks like we cant do v=v+v[::-1] on lists?!

--- repository/fastino_sawtooth.py
+++ repository/fastino_sawtooth.py
@@ -1,7 +1,3 @@
-import sys, os
-sys.path.append(
-    os.path.join(os.path.dirname(__file__), "..")
-)  # link to repository root
 from artiq.experiment import *
 
 from utils.surpress_missing_imports import *
@@ -10,8 +6,10 @@ from utils.wait_for_enter import is_enter_pressed
 # This code outputs a ramp wave on a single channel of the fastino
 # The wave ramps from -10v to 10v with a frequency of 1.28kHz
 
+
 class Fastino_Ramp_Generator(EnvExperiment):
     """fastino: Ramp Generator"""
+
     def build(self):
         self.setattr_device("core")
         self.setattr_device("fastino")
@@ -21,17 +19,31 @@ class Fastino_Ramp_Generator(EnvExperiment):
     @kernel  # this code runs on the FPGA
     def run(self):
         self.core.reset()
-        self.core.break_realtime()
 
         self.fastino.init()
         delay(200 * us)
         self.fastino.set_leds(0b01010101)
         delay(100 * us)
 
-        voltages = [i/100. for i in range(-999,1000)]
-        
+        voltages = [i / 10.0 for i in range(-100, 100)]
+        # voltages = voltages[::-1]
+        gap = self.core.seconds_to_mu(1 * ms)
+
         print("Press ENTER to cancel.")
+        # keep this low to avoid long response times due to backed up FIFOs
+        # but long enough to poll for enter on the host otherwise we underflow
+        time_to_poll = self.core.seconds_to_mu(100 * ms)
+        self.core.break_realtime()
+        delay_mu(time_to_poll)
+
         while not is_enter_pressed():
-            for v in voltages:
-                self.fastino.set_dac(0, v)
-                delay(100 * us)
+            # fill the FIFOs until we build enough of a buffer
+            fill_to = self.core.get_rtio_counter_mu() + time_to_poll
+            while now_mu() < fill_to:
+                # contribute more events to fill the timeline
+                for v in voltages:
+                    self.fastino.set_dac(0, v)
+                    delay_mu(gap)
+
+        self.fastino.set_leds(0b00000000)
+        self.fastino.set_dac(0,0.0)
