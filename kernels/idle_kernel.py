55646296d8b5d7d9cbec3f2673f41055c42805e4
Harry Kendell <harry@kendell.co.uk>
2024-05-03 12:05:22 +0100

    cleaned up comments

--- kernels/idle_kernel.py
+++ kernels/idle_kernel.py
@@ -1,12 +1,7 @@
-"""
-The idle sequence for the experiment.
-"""
-
 from artiq.experiment import *
 
-
 class Idle(EnvExperiment):
-
+# The idle sequence for the experiment.
     def build(self):
         self.setattr_device("core")
         self.setattr_device("led0")
@@ -17,6 +12,7 @@ class Idle(EnvExperiment):
         while self.core.get_rtio_counter_mu() < start_time:
             pass
         self.core.reset()
+        self.core.break_realtime()
     
         while True:
             self.led0.pulse(250*ms)
