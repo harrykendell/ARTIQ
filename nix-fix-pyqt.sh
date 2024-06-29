4027b7f63cd063bf7d611e0e27131c2acc82b69f
harrykendell <harry@kendell.co.uk>
2024-06-29 18:15:14 +0100

    We can now use PyQt in the nix shell !!!!!

---
+++ nix-fix-pyqt.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+# This script sets the PyQt5 environment variables that are missing in the Nix build.
+# Without these variables PyQt5 programs will crash unless explicitly hooked like artiq_dashboard.
+if [ ${_} != ${0} ]; then
+    export QT_PLUGIN_PATH=$(find /nix/store -maxdepth 4 -mindepth 4 -wholename '/*/lib/qt-5.15.12/plugins' | paste -sd ":" -)
+unset XDG_SESSION_TYPE
+export LANG=C
+else
+    RED='\033[0;31m'
+    NC='\033[0m' # No Color
+    echo -e "This script must be run with the dot space script syntax, as in \n${RED}. nix-fix-pyqt.sh${NC}\notherwise it cannot export environment variables into your shell."
+fi
+
+
