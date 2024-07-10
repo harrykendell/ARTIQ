880583e75cef5cca4382335c0f78b3ce9c337d1f
harrykendell <harry@kendell.co.uk>
2024-07-10 14:40:44 +0100

    ndscan test

---
+++ repository/tests/test_ndscan.py
@@ -0,0 +1,23 @@
+from artiq.experiment import EnvExperiment
+from ndscan.experiment import ExpFragment
+from ndscan.experiment import ResultChannel
+from ndscan.experiment.entry_point import create_and_run_fragment_once
+from ndscan.experiment.entry_point import make_fragment_scan_exp
+
+
+class MyExpFragment(ExpFragment):
+    def build_fragment(self):
+        self.setattr_result("foo")
+        self.foo: ResultChannel
+
+    def run_once(self):
+        self.foo.push(123)
+
+
+class MyEnvExperiment(EnvExperiment):
+    def run(self):
+        results = create_and_run_fragment_once(self, MyExpFragment)
+        print(results["foo"])
+
+
+MyExpFragmentScan = make_fragment_scan_exp(MyExpFragment)
