87dab044ad18b6912c001fa9214a3e14023dee37
harrykendell <harry@kendell.co.uk>
2024-07-12 14:48:56 +0100

    few improvements to almazny

--- repository/almazny.py
+++ repository/almazny.py
@@ -1,7 +1,8 @@
 from artiq.experiment import *
-from artiq.language import MHz, delay, ms
+from artiq.language import MHz, delay, ms, dB
 from artiq.coredevice.adf5356 import ADF5356
 from artiq.coredevice.almazny import AlmaznyChannel
+from artiq.coredevice.core import Core
 
 class TestMirny(EnvExperiment):
     '''Test Mirny and Almazny.
@@ -10,45 +11,42 @@ class TestMirny(EnvExperiment):
     We also try to set `frequency` on the Mirny channel 1.
     '''
     def build(self):
-        self.core = self.get_device("core")
+        self.core: Core = self.get_device("core")
         self.mirny: ADF5356 = self.get_device("mirny_ch0")
-        self.almazny: AlmaznyChannel = self.get_device("almazny_ch0")
+        self.almazny: AlmaznyChannel = self.get_device("almazny_ch0")    
 
         self.base_mirny: ADF5356 = self.get_device("mirny_ch1")
+        self.base_alamzny: AlmaznyChannel = self.get_device("almazny_ch1")
 
         self.setattr_argument("frequency", NumberValue(unit="MHz", default=6800.0*MHz, precision=3, min=53.125*MHz, max=13600.0*MHz, type="float"),tooltip="Frequency to set on the Almazny with frequency/2 on the Mirny.")
-
+        self.frequency_2 = self.frequency/2.0 if self.frequency else 0.0*MHz
     @kernel
     def run(self):
         self.core.reset()
 
-        # init Mirnys
+        # init Mirny CPLD - shared by all Mirny channels
         self.mirny.cpld.init()
+
+        # init Mirny channel 0
+        self.core.break_realtime()
         self.mirny.init()
-
-        delay(10 * ms)
-        self.base_mirny.cpld.init()
-        self.base_mirny.init()
-
-
-        # max their output powers
-        self.mirny.set_att(0.)
+        self.mirny.set_att(0.0*dB)
         self.mirny.sw.on()
         self.core.break_realtime()
+        self.mirny.set_frequency(self.frequency)
+        delay(100 * ms)
+        self.core.break_realtime()
+        self.almazny.set(att=0.0*dB, enable=True, led=True)
 
-        self.base_mirny.set_att(0.)
+        # second Mirny channel
+        self.core.break_realtime()
+        self.base_mirny.init()
+        self.base_mirny.set_att(0.0*dB)
         self.base_mirny.sw.on()
         self.core.break_realtime()
-
-
-        # set freqs
-        self.mirny.set_frequency(self.frequency / 2)
-        self.almazny.set(att=0., enable=True, led=True)
-        self.base_mirny.set_frequency(self.frequency)
-
-        inspect(self)
-
-@rpc(flags={"async"})
-def inspect(self):
-        print("mirny_ch0 info: {}".format(self.mirny.info()))
-        print("mirny_ch1 info: {}".format(self.base_mirny.info()))
+        self.base_mirny.set_frequency(self.frequency_2)
+        delay(100 * ms)
+        self.core.break_realtime()
+        self.base_alamzny.set(att=0.0*dB, enable=True, led=True)
+        
+        print(self.mirny.info())
