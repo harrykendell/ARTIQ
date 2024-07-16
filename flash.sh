611ddea312ff44e4dcbbaa1fd285f91d6e665728
harrykendell <harry@kendell.co.uk>
2024-07-16 13:59:51 +0100

    improved crate flashing

--- flash.sh
+++ flash.sh
@@ -8,19 +8,19 @@ GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
 # check for required build tool
-if [[ -d /opt/Xilinx ]]; then
+if [[ -d /opt/Xilinx/Vivado/2022.2 ]]; then
     echo -e "${GREEN}Vivado 2020.2 found, flashing artiq crate${NC}"
     cd src/artiq
-
-    # build firmware
+    
+    # build firmware - 8.0+
     nix develop --command bash -c "export PYTHONPATH=`pwd`:$PYTHONPATH"
     nix develop --command bash -c "python -m artiq.gateware.targets.kasli ../../crate\ config/bu2402001.json"
 
     # flash crate
-    cp -TR artiq_kasli/bu2402001 ../../crate\ config/bu2402001_github
+    cp -TR artiq_kasli/bu2402001 ../../crate\ config/bu2402001_current
     nix develop --command bash -c "artiq_flash --srcbuild -d artiq_kasli/bu2402001"
 
-    echo -e "${GREEN}Flashing complete, firmware updated in 'crate config/bu2402001_github'${NC}"
+    echo -e "${GREEN}Flashing complete, firmware updated in 'crate config/bu2402001_current'${NC}"
 else
     echo -e "${RED}Vivado 2020.2 not found, cannot build and flash artiq crate${NC}"
 fi
