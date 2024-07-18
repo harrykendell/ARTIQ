da4ff733cd6032129162355d18e02356f4668a31
harrykendell <harry@kendell.co.uk>
2024-07-18 13:53:43 +0100

    avoid another weird error message

--- nix-fix-pyqt.sh
+++ nix-fix-pyqt.sh
@@ -3,12 +3,14 @@
 # Without these variables PyQt5 programs will crash unless explicitly hooked like artiq_dashboard.
 if [ ${_} != ${0} ]; then
     export QT_PLUGIN_PATH=$(find /nix/store -maxdepth 4 -mindepth 4 -wholename '/*/lib/qt-5.15.14/plugins' | paste -sd ":" -)
-unset XDG_SESSION_TYPE
-export LANG=C
+    export QT_XCB_GL_INTEGRATION=none
+    unset XDG_SESSION_TYPE
+    export LANG=C
 else
     RED='\033[0;31m'
     NC='\033[0m' # No Color
-    echo -e "This script must be run with the dot space script syntax, as in \n${RED}. nix-fix-pyqt.sh${NC}\notherwise it cannot export environment variables into your shell."
+    echo -e "This script must be run with the dot space script syntax, as in \n${RED}. nix-fix-pyqt.sh${NC}"
+    echo -e "Otherwise it cannot export environment variables into your shell"
 fi
 
 
