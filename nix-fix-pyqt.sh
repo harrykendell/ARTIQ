0884506d7bfb15a1e5ec43615bbcab37fd791ba8
harrykendell <harry@kendell.co.uk>
2025-01-01 18:41:56 +0000

    commented out fixes that no longer work

--- nix-fix-pyqt.sh
+++ nix-fix-pyqt.sh
@@ -2,9 +2,13 @@
 # This script sets the PyQt5 environment variables that are missing in the Nix build.
 # Without these variables PyQt5 programs will crash unless explicitly hooked like artiq_dashboard.
 if [ ${_} != ${0} ]; then
-    export QT_PLUGIN_PATH=$(find /nix/store -maxdepth 4 -mindepth 4 -wholename '/*/lib/qt-5.15.15/plugins' | paste -sd ":" -)
-    export QT_XCB_GL_INTEGRATION=none
-    unset XDG_SESSION_TYPE
+    # export QT_PLUGIN_PATH=$(find /nix/store -maxdepth 4 -mindepth 4 -wholename '/*/lib/qt-5.15.15/plugins' | paste -sd ":" -)
+    # export QT_XCB_GL_INTEGRATION=none
+    # unset XDG_SESSION_TYPE
+
+    # This avoids the error: qt.qpa.plugin: Could not find the Qt platform plugin "wayland" in ""
+    export QT_QPA_PLATFORM=xcb
+    # this avoids the error: Gtk-WARNING **: 18:37:02.039: Locale not supported by C library. Using the fallback 'C' locale.
     export LANG=C
 else
     RED='\033[0;31m'
