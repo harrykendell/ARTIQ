f10327a12f11d6680358cbfd194e3ce44812ad5a
harrykendell <harry@kendell.co.uk>
2025-01-07 17:52:22 +0000

    allow use from another directory but fix it behind the scenes

--- run_artiq.sh
+++ run_artiq.sh
@@ -1,2 +1,17 @@
 #! /bin/bash
+
+# The directory where the script is located
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+
+# Check if the script is run from the expected directory
+if [ "$SCRIPT_DIR" != "$(pwd)" ]; then
+    # Warn the user if the script is not run from the expected directory
+    RED='\033[0;31m'
+    NC='\033[0m' # No Color
+
+    echo -e "${RED}Error: This script must be run from its directory $SCRIPT_DIR"
+    echo -e "Changing directory to $SCRIPT_DIR${NC}"
+    cd $SCRIPT_DIR
+fi
+
 nix shell --command bash -c '. ./scripts/nix-fix-pyqt.sh ; (python ./ThorlabsPM/ThorlabsPM.py &) ; artiq_session -d="-p=ndscan.dashboard_plugin"'
