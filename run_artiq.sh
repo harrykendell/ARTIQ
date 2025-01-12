8a309e870b8866e408c27eb3a4981fc4c0b6b1f6
harrykendell <harry@kendell.co.uk>
2025-01-12 23:57:20 +0000

    fix up to still run artiq on non-server computers

--- run_artiq.sh
+++ run_artiq.sh
@@ -14,6 +14,7 @@ if [ "$SCRIPT_DIR" != "$(pwd)" ]; then
     cd $SCRIPT_DIR
 fi
 
+
 #  PyQt5 fix
 FIX=". ./scripts/nix-fix-pyqt.sh"
 
@@ -21,9 +22,18 @@ FIX=". ./scripts/nix-fix-pyqt.sh"
 TLPM="(python ./ThorlabsPM/ThorlabsPM.py &)"
 
 # ARTIQ
-IP_ADDRESS=137.222.69.28
-ARTIQ="artiq_session -d=\"-s=$IP_ADDRESS\" -d=\"-p=ndscan.dashboard_plugin\" -m=\"--bind=$IP_ADDRESS\" -c=\"--bind=$IP_ADDRESS\" -c=\"-s=$IP_ADDRESS\""
+SERVER_ADDRESS=137.222.69.28
+ARTIQ="artiq_session -d=\"-s=$SERVER_ADDRESS\" -d=\"-p=ndscan.dashboard_plugin\" -m=\"--bind=$SERVER_ADDRESS\" -c=\"--bind=$SERVER_ADDRESS\" -c=\"-s=$SERVER_ADDRESS\""
 
-nix shell --command bash -c "$FIX ; $TLPM ; $ARTIQ"
 
-# nix shell --command bash -c '. ./scripts/nix-fix-pyqt.sh ; (python ./ThorlabsPM/ThorlabsPM.py &) ; artiq_session -d="-s=137.222.69.28" -d="-p=ndscan.dashboard_plugin" -m="--bind=137.222.69.28" -c="--bind=137.222.69.28" -c="-s=137.222.69.28"'
+# check if we are running on the artiq server or not
+IP_ADDRESSES=$(hostname -I 2>/dev/null);
+if [[$IP_ADDRESSES == *"$SERVER_ADDRESS"*]]; then
+    echo -e "${GREEN}Running on the ARTIQ server${NC}"
+    nix shell --command bash -c "$FIX ; $TLPM ; $ARTIQ"
+else
+    echo -e "${RED}Not running on the ARTIQ server${NC}"
+    artiq_dashboard --server="$SERVER_ADDRESS" -p ndscan.dashboard_plugin
+fi
+
+# nix shell --command bash -c '. ./scripts/nix-fix-pyqt.sh ; (python ./ThorlabsPM/ThorlabsPM.py &) ; artiq_session -d="-s=137.222.69.28" -d="-p=ndscan.dashboard_plugin" -m="--bind=137.222.69.28" -c="--bind=137.222.69.28"'
