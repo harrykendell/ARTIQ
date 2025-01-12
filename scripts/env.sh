bef0a127b44f02b741a18bb0e33900804039fa70
harrykendell <harry@kendell.co.uk>
2025-01-12 15:26:58 +0000

    track changes when setting up artiq environment

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,9 +1,15 @@
 #! /bin/bash
 # This script installs the modules as per flake.nix src directory which is gitignored
-# NB you may need to run this manually step by step
-
+# NB you may need to run this manually step by step if you have issues
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+pip list | sort >start_pip_list.txt
 # Windfreak
 pip install windfreak==0.3.0
+
+# pco
+pip install pco==2.3.0
 
 # Booster
 pip install -e "git+https://github.com/quartiq/booster.git@a1f83b63180511ecd68f88a04621624941d17a41#subdirectory=py/&egg=booster"
@@ -26,13 +32,21 @@ pip install -e "git+https://github.com/OxfordIonTrapGroup/ndscan.git#egg=ndscan"
 # pydantic
 pip install pydantic
 
-# Check the installed packages - we should see if any of the above failed
-echo -e "\n\nInstalled packages:\n"
-pip list | grep 'windfreak\|booster\|miniconf-mqtt\|oitg\|oxart-devices\|artiq\|ndscan\|pydantic'
+############        POST INSTALLATION CHECKS        ############
+pip list | sort >end_pip_list.txt
 
-# conda installed artiq doesnt properly link the site-packages to the python3.10 site-packages
-# cd ~/anaconda3/envs/artiq/site-packages
-# for file in *
-# do
-#  ln -sf ../../../site-packages/$file ../lib/python3.10/site-packages/$file
-# done
+echo -e "\n\n${GREEN}Installed packages:${NC}\n"
+pip list | grep 'windfreak\|pco\|booster\|miniconf-mqtt\|oitg\|oxart-devices\|artiq\|ndscan\|pydantic'
+
+echo -e "\n\n${GREEN}Diff:${NC}\n"
+diff --color=always start_pip_list.txt end_pip_list.txt
+
+rm start_pip_list.txt end_pip_list.txt
+
+# Conda installed artiq doesn't properly link the site-packages to the python3.10 site-packages
+# You may need to run the following commands
+#  cd ~/anaconda3/envs/artiq/site-packages
+#  for file in *
+#  do
+#   ln -sf ../../../site-packages/$file ../lib/python3.10/site-packages/$file
+#  done
