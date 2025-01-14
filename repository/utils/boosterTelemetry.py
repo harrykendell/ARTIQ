d3af5299d91288fd580a5f073a797e9befbd87f3
harrykendell <harry@kendell.co.uk>
2025-01-14 00:52:27 +0000

    control more aspects of the Booster directly

--- repository/utils/boosterTelemetry.py
+++ repository/utils/boosterTelemetry.py
@@ -2,11 +2,13 @@ import asyncio, aiomqtt
 from PyQt5.QtCore import QThread, pyqtSignal, QObject
 
 class TelemetryWorker(QObject):
+    server = "137.222.69.28"
+
     telemetry_received = pyqtSignal(int,str)
 
     async def listen(self):
         # we want to listen to updates of channel[0-7]/[state,input power, output power]
-        async with aiomqtt.Client("137.222.69.28") as client:
+        async with aiomqtt.Client(TelemetryWorker.server) as client:
             await client.subscribe("dt/sinara/booster/fc-0f-e7-23-77-30/telemetry/#")
             client._on_message = self.handle_message
             async for message in client.messages:
@@ -18,6 +20,21 @@ class TelemetryWorker(QObject):
         ch = int(message.topic.value[-1])
         data = message.payload.decode()
         self.telemetry_received.emit(ch,data)
+
+    async def _set_telem_period(self, period=1):
+        ''' Set the period of the telemetry updates'''
+        async with aiomqtt.Client(TelemetryWorker.server) as client:
+            await client.publish("dt/sinara/booster/fc-0f-e7-23-77-30/settings/telemetry_period", str(period))
+
+    async def set_interlock(self, ch, db=35.0):
+        ''' Set the interlock state of a channel ch to db'''
+        async with aiomqtt.Client(TelemetryWorker.server) as client:
+            await client.publish(f"dt/sinara/booster/fc-0f-e7-23-77-30/settings/channel/{ch}/output_interlock_threshold", str(db))
+
+    async def set_state(self, ch, state='Enabled'):
+        ''' Set the state of a channel ch to state ['Off','Powered','Enabled']'''
+        async with aiomqtt.Client(TelemetryWorker.server) as client:
+            await client.publish(f"dt/sinara/booster/fc-0f-e7-23-77-30/settings/channel/{ch}/state", str(state))
 
     def run(self):
         asyncio.run(self.listen())
@@ -32,3 +49,25 @@ class BoosterTelemetry(QThread):
 
     def run(self):
         self.worker.run()
+
+    def set_telem_period(self, period=1):
+        asyncio.run(self.worker._set_telem_period(period))
+
+    def set_interlock(self, ch, db=35.0):
+        asyncio.run(self.worker.set_interlock(ch, db))
+
+    def enable_channel(self, ch):
+        asyncio.run(self.worker.set_state(ch, 'Powered'))
+        asyncio.run(self.worker.set_state(ch, 'Enabled'))
+
+    def disable_channel(self, ch):
+        asyncio.run(self.worker.set_state(ch, 'Off'))
+
+if __name__ == "__main__":
+    def callback(ch, data):
+        print(f"Received telemetry from channel {ch}: {data}")
+    worker = BoosterTelemetry(callback)
+    worker.set_telem_period(1)
+    worker.run()
+    while True:
+        pass
