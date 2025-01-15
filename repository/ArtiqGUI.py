0783f9e1f78c26ca85ab9ae1ba1c07b1ab1db802
harrykendell <harry@kendell.co.uk>
2025-01-15 14:46:30 +0000

    show data in text fields

--- repository/ArtiqGUI.py
+++ repository/ArtiqGUI.py
@@ -16,17 +16,19 @@ This is achieved by running a Subscriber in the background that listens for chan
 widgets can register to be updated for specific datasets
 """
 
+import sys, logging
+
+# Data subscriptions
 from sipyco.pc_rpc import AsyncioClient
-from sipyco.sync_struct import Subscriber, process_mod
+from sipyco.sync_struct import Subscriber
 from sipyco.asyncio_tools import atexit_register_coroutine, atexit
 import asyncio
 from qasync import QEventLoop
 from utils.boosterTelemetry import BoosterTelemetry
 
-from PyQt5.QtWidgets import QApplication, QLabel, QWidget
+# GUI
+from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QVBoxLayout, QTextEdit
 from PyQt5.QtGui import QIcon
-
-import logging
 from artiq.gui.scientific_spinbox import ScientificSpinBox
 
 
@@ -34,7 +36,13 @@ class Client:
     # Holds the connection to everything we want to monitor
     # Artiq, Booster, DLCPro, etc
 
-    def __init__(self, app: QApplication, server="137.222.69.28", port_control=3251, port_notify=3250):
+    def __init__(
+        self,
+        app: QApplication,
+        server="137.222.69.28",
+        port_control=3251,
+        port_notify=3250,
+    ):
         self.server = server
         self.port_control = port_control
         self.port_notify = port_notify
@@ -43,13 +51,16 @@ class Client:
         asyncio.set_event_loop(self.loop)
         atexit.register(self.loop.close)
 
-        # self.booster_init()
+        self.booster_db = dict()
+        self.booster_init()
 
         self.rpc_clients = dict()
         self.rpc_init()
 
         self.sub_clients = dict()
+        self.schedule_db = dict()
         self.schedule_init()
+        self.datatset_db = dict()
         self.dataset_init()
 
     def rpc_init(self):
@@ -63,7 +74,6 @@ class Client:
             self.rpc_clients[target] = client
 
     def dataset_init(self):
-        self.datatset_db = dict()
         self.dataset_callbacks = dict()
 
         def create_d(data):
@@ -74,12 +84,13 @@ class Client:
         def modify_d(mod):
 
             if mod["action"] != "setitem":
-                if mod["action"] != "init":
-                    logging.error(
-                        f"We cannot handle {mod['action']} on datasets"
-                    )
+                if mod["action"] == "init":
+                    for key in self.dataset_callbacks.keys():
+                        [cb() for cb in self.dataset_callbacks[key]]
+                else:
+                    logging.error(f"We cannot handle {mod['action']} on datasets")
                 return
-                
+
             logging.debug(f"New dataset mod")
             # we want to trigger on the key or a subpath of the key
             # i.e. if we have a key "foo" we want to trigger on mod["key"] = "foo"/"foo.bar" but not "foobar"
@@ -88,21 +99,18 @@ class Client:
                 if key == mod["key"] or mod["key"].startswith(key + ".") or key == "*":
                     [cb() for cb in self.dataset_callbacks[key]]
             return
-        
+
         subscriber = Subscriber(
             "datasets",
             target_builder=create_d,
             notify_cb=modify_d,
             disconnect_cb=None,
         )
-        self.loop.run_until_complete(
-            subscriber.connect(self.server, self.port_notify)
-        )
+        self.loop.run_until_complete(subscriber.connect(self.server, self.port_notify))
         atexit_register_coroutine(subscriber.close, loop=self.loop)
         self.sub_clients["datasets"] = subscriber
 
     def schedule_init(self):
-        self.schedule_db = dict()
         self.schedule_callbacks = []
 
         def create_s(data):
@@ -120,44 +128,103 @@ class Client:
             notify_cb=modify_s,
             disconnect_cb=None,
         )
-        self.loop.run_until_complete(
-            subscriber.connect(self.server, self.port_notify)
-        )
+        self.loop.run_until_complete(subscriber.connect(self.server, self.port_notify))
         atexit_register_coroutine(subscriber.close, loop=self.loop)
         self.sub_clients["schedule"] = subscriber
 
+    # register to be called when new data is available
+    # cb()
     def register_dataset_callback(self, key, cb):
         if key not in self.dataset_callbacks:
             self.dataset_callbacks[key] = []
         self.dataset_callbacks[key].append(cb)
+        cb()
 
+    # register to be called when new data is available
+    # cb()
     def register_schedule_callback(self, cb):
-        # register to be called when new data is available
-        # cb()
         self.schedule_callbacks.append(cb)
+        cb()
 
     def booster_init(self):
-        self.booster = BoosterTelemetry(self.booster_callback)
-        self.booster_state = dict()
+        def booster_callback(ch, data):
+            logging.debug(f"\nnew Booster data ({ch}):\n{data}")
+            self.booster_db[ch] = data
+            for cb in self.booster_callbacks[ch]:
+                cb()
+
+        self.booster = BoosterTelemetry(booster_callback)
         self.booster_callbacks = [[]] * 8
         self.booster.start()
         self.booster.set_telem_period(5)
 
-    def booster_callback(self, ch, data):
-        logging.debug(f"\nnew Booster data ({ch}):\n{data}")
-        self.booster_state[ch] = data
-        for cb in self.booster_callbacks[ch]:
-            cb(data,ch)
-
+    # register to be called when new data is available
+    # cb()
     def register_booster_callback(self, ch, cb):
-        # register to be called when new data is available
-        # cb(data, ch)
         if ch == "*":
             [self.booster_callbacks[i].append(cb) for i in range(8)]
         elif ch in range(8):
             self.booster_callbacks[ch].append(cb)
         else:
             logging.error(f"Invalid booster channel {ch}")
+
+
+class UI(QWidget):
+    def __init__(self, client: Client):
+        super().__init__()
+
+        self.client = client
+
+        self.setWindowTitle("ARTIQ GUI")
+        self.setGeometry(100, 100, 800, 600)
+
+        self.client = client
+        self.inutUI()
+
+        self.register_callbacks()
+
+    def inutUI(self):
+        layout = QVBoxLayout()
+
+        self.spinbox = ScientificSpinBox()
+        layout.addWidget(self.spinbox)
+
+        self.dataset_label = QLabel("Dataset:")
+        self.dataset_text = QTextEdit()
+        self.dataset_text.setReadOnly(True)
+        layout.addWidget(self.dataset_label)
+        layout.addWidget(self.dataset_text)
+        self.update_dataset()
+
+        self.schedule_label = QLabel("Schedule:")
+        self.schedule_text = QTextEdit()
+        self.schedule_text.setReadOnly(True)
+        layout.addWidget(self.schedule_label)
+        layout.addWidget(self.schedule_text)
+        self.update_schedule()
+
+        self.booster_label = QLabel("Booster:")
+        self.booster_text = QTextEdit()
+        self.booster_text.setReadOnly(True)
+        layout.addWidget(self.booster_label)
+        layout.addWidget(self.booster_text)
+        self.update_booster()
+
+        self.setLayout(layout)
+
+    def update_dataset(self):
+        self.dataset_text.setText(str(self.client.datatset_db))
+
+    def update_schedule(self):
+        self.schedule_text.setText(str(self.client.schedule_db))
+
+    def update_booster(self):
+        self.booster_text.setText(str(self.client.booster_db))
+
+    def register_callbacks(self):
+        self.client.register_dataset_callback("*", self.update_dataset)
+        self.client.register_schedule_callback(self.update_schedule)
+        self.client.register_booster_callback("*", self.update_booster)
 
 def main():
     app = QApplication(["Test GUI"])
@@ -167,17 +234,13 @@ def main():
     app.setApplicationName("ARTIQ GUI")
 
     client = Client(app)
+    ui = UI(client)
+    ui.show()
 
-    spinbox = ScientificSpinBox()
-    spinbox.show()
+    sys.exit(app.exec())
 
-    client.register_dataset_callback("*", lambda: print(f"New dataset:\n{client.datatset_db}"))
-    client.register_schedule_callback(lambda: print(f"New schedule:\n", client.schedule_db))
-    # client.register_booster_callback("*", lambda data, ch: print(f"New Booster data Ch{ch}:\n{data}"))
-
-    app.exec()
 
 if __name__ == "__main__":
     logger = logging.getLogger()
-    logger.setLevel(logging.DEBUG)
+    logger.setLevel(logging.INFO)
     main()
