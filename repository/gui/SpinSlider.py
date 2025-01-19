495fa9f0d7aadad85165ef737b3c8bd50241665a
harrykendell <harry@kendell.co.uk>
2025-01-19 23:21:26 +0000

    Basic GUI elements

---
+++ repository/gui/SpinSlider.py
@@ -0,0 +1,177 @@
+# A PyQt Widget that combines a SpinBox and a Slider
+#
+#   - ScientificSpinBox
+#   - Synchronized slider and spinbox
+#   - Signal emmited when the value changes
+#   - Units for the minimum and maximum labels
+
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QLabel,QSlider, QDoubleSpinBox
+from PyQt5.QtCore import Qt, pyqtSignal as Signal
+from artiq.gui.scientific_spinbox import ScientificSpinBox
+from artiq.gui.tools import disable_scroll_wheel
+
+import logging
+from math import floor, ceil, pi
+
+logger = logging.getLogger(__name__)
+
+# only inherit from QWidget as we need to keep state synced
+class SpinAndSlider(QWidget):
+    valueChanged = Signal(float)
+
+    def __init__(self, parent=None, min= 0., max = 100., unit="", initialValue = None, label=""):
+        super().__init__(parent)
+        # ensure min < max
+        self.min, self.max = (min, max) if min < max else (max, min)
+        self.num_decimals = 2
+
+        self.label = label
+        self.unit = unit
+
+        self.current_value = min if initialValue is None else initialValue
+
+        self.initUI()
+        self.setStep(1)
+
+    def initUI(self):
+        layout = QVBoxLayout()
+
+        self.title = QLabel(f"<b>{self.label}<b>")
+
+        self.spin = ScientificSpinBox()
+        disable_scroll_wheel(self.spin)
+        self.spin.setDecimals(3)
+
+        self.spin.setMinimum(self.min)
+        self.spin.setMaximum(self.max)
+        self.spin.setValue(self.current_value)
+        # self.spin.valueChanged.connect(self.spinChanged)
+        # only update when the user has finished editing
+        self.spin.editingFinished.connect(lambda: self.spinChanged(self.spin.value()))
+
+        self.slider = QSlider(Qt.Horizontal)
+        self.slider.setMinimum(floor(self.min*10**self.num_decimals))
+        self.slider.setMaximum(ceil(self.max*10**self.num_decimals))
+        self.slider.setValue(int(self.current_value*10**self.num_decimals))
+        self.slider.valueChanged.connect(self.sliderChanged)
+
+        self.min_label = QLabel(f"{self.spin.textFromValue(self.min)} <b>{self.unit}</b>")
+        self.max_label = QLabel(f"{self.spin.textFromValue(self.max)} <b>{self.unit}</b>")
+
+        vbox = QVBoxLayout()
+        layout.addLayout(vbox)
+
+        topbox = QHBoxLayout()
+        topbox.addWidget(self.title)
+        # topbox.addStretch()
+        topbox.addWidget(self.spin)
+        vbox.addLayout(topbox)
+
+        bottombox = QHBoxLayout()
+        bottombox.addWidget(self.min_label)
+        bottombox.addWidget(self.slider)
+        bottombox.addWidget(self.max_label)
+        vbox.addLayout(bottombox)
+
+        self.setLayout(layout)
+
+    def spinChanged(self, value):
+        logger.debug("Spin changed to %s", value)
+        self.current_value = value
+        self.slider.setValue(int(value*10**self.num_decimals))
+        self.valueChanged.emit(value)
+
+    def sliderChanged(self, value):
+        value = value/10**self.num_decimals
+        logger.debug("Slider changed to %s", value)
+
+        # NB we could be out of range as the slider is integer only
+        if value < self.min:
+            value = self.min
+            logger.debug("Clamping to min")
+        elif value > self.max:
+            value = self.max
+            logger.debug("Clamping to max")
+
+        self.current_value = value
+        self.spin.setValue(float(value))
+
+        # remove focus
+        self.slider.setFocus()
+
+        self.valueChanged.emit(value)
+
+    def setUnit(self, unit=""):
+        self.unit = unit
+        self.min_label.setText(f"{self.spin.textFromValue(self.min)} <b>{self.unit}</b>")
+        self.max_label.setText(f"{self.spin.textFromValue(self.max)} <b>{self.unit}</b>")
+
+    def setValue(self, value):
+        self.current_value = value
+        self.spin.setValue(value)
+        self.slider.setValue(int(value*10**self.num_decimals))
+
+    def setRange(self, min, max):
+        self.min, self.max = (min, max) if min < max else (max, min)
+
+        self.spin.setMinimum(min)
+        self.spin.setMaximum(max)
+        self.slider.setMinimum(min*10**self.num_decimals)
+        self.slider.setMaximum(max*10**self.num_decimals)
+
+        self.min_label.setText(f"{self.spin.textFromValue(self.min)} <b>{self.unit}</b>")
+        self.max_label.setText(f"{self.spin.textFromValue(self.max)} <b>{self.unit}</b>")
+
+        # clamp the current value to the new range
+        self.setValue(min(max(self.current_value, min), max))
+
+    def setLabel(self, label):
+        self.title.setText(label)
+
+    def setDecimals(self, decimals):
+        self.num_decimals = decimals
+        self.spin.setDecimals(decimals)
+
+        # update the slider range
+        self.slider.setMinimum(self.min*10**decimals)
+        self.slider.setMaximum(self.max*10**decimals)
+        self.slider.setValue(int(self.current_value*10**decimals))
+
+    def setStep(self, step):
+        self.spin.setSingleStep(step)
+        self.slider.setSingleStep(step)
+
+    def setSuffix(self, suffix):
+        self.spin.setSuffix(suffix)
+
+    def setEnabled(self, value):
+        self.spin.setEnabled(value)
+        self.slider.setEnabled(value)
+
+    def value(self):
+        return self.current_value
+
+    def minimum(self):
+        return self.min
+
+    def maximum(self):
+        return self.max
+
+    def setMinimum(self, min):
+        self.setRange(min, self.max)
+
+    def setMaximum(self, max):
+        self.setRange(self.min, max)
+
+if __name__ == "__main__":
+    import sys
+    from PyQt5.QtWidgets import QApplication
+
+    logger.setLevel(logging.DEBUG)
+
+    app = QApplication(sys.argv)
+    ex = SpinAndSlider(min=0, max=1000, initialValue=pi, label="Frequency", unit="MHz")
+    ex.show()
+
+    ex.valueChanged.connect(lambda x: print("Value changed to", x))
+    sys.exit(app.exec_())
---
+++ repository/gui/Switch.py
@@ -0,0 +1,152 @@
+"""
+MIT License
+
+Copyright (c) 2021 Parsa.py
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+from PyQt5.QtCore import (
+    Qt,
+    QPoint,
+    pyqtSlot,
+    pyqtProperty,
+    QPropertyAnimation,
+    QEasingCurve,
+)
+from PyQt5.QtWidgets import QWidget, QCheckBox
+from PyQt5.QtGui import QPainter, QColor
+
+
+def take_closest(num, collection):
+    return min(collection, key=lambda x: abs(x - num))
+
+
+class SwitchCircle(QWidget):
+    def __init__(
+        self, parent, move_range: tuple, color, animation_curve, animation_duration
+    ):
+        super().__init__(parent=parent)
+        self.color = color
+        self.move_range = move_range
+        self.animation = QPropertyAnimation(self, b"pos")
+        self.animation.setEasingCurve(animation_curve)
+        self.animation.setDuration(animation_duration)
+
+    def paintEvent(self, event):
+        painter = QPainter()
+        painter.begin(self)
+        painter.setRenderHint(QPainter.HighQualityAntialiasing)
+        painter.setPen(Qt.NoPen)
+        painter.setBrush(QColor(self.color))
+        painter.drawEllipse(0, 0, 16, 16)
+        painter.end()
+
+    def set_color(self, value):
+        self.color = value
+        self.update()
+
+class SwitchControl(QCheckBox):
+
+    def __init__(
+        self,
+        parent=None,
+        bg_color="gray",
+        circle_color="#DDD",
+        active_color="green",
+        animation_curve=QEasingCurve.InOutExpo,
+        animation_duration=50,
+        checked: bool = False,
+    ):
+        super().__init__(parent=parent)
+        self.setFixedSize(40, 20)
+        self.bg_color = bg_color
+        self.circle_color = circle_color
+        self.animation_curve = animation_curve
+        self.animation_duration = animation_duration
+        self.__circle = SwitchCircle(
+            self,
+            (2, self.width() - 20),
+            self.circle_color,
+            self.animation_curve,
+            self.animation_duration,
+        )
+        self.active_color = active_color
+        if checked:
+            self.__circle.move(self.width() - 20, 2)
+            self.setChecked(True)
+        elif not checked:
+            self.__circle.move(3, 2)
+            self.setChecked(False)
+        self.animation = QPropertyAnimation(self.__circle, b"pos")
+        self.animation.setEasingCurve(animation_curve)
+        self.animation.setDuration(animation_duration)
+
+    def start_animation(self, checked):
+        self.animation.stop()
+        self.animation.setStartValue(self.__circle.pos())
+        if checked:
+            self.animation.setEndValue(QPoint(self.width() - 20, self.__circle.y()))
+            self.setChecked(True)
+        if not checked:
+            self.animation.setEndValue(QPoint(3, self.__circle.y()))
+            self.setChecked(False)
+        self.animation.start()
+
+    def paintEvent(self, event):
+        painter = QPainter()
+        painter.begin(self)
+        painter.setRenderHint(QPainter.HighQualityAntialiasing)
+        painter.setPen(Qt.NoPen)
+        if not self.isChecked():
+            painter.setBrush(QColor(self.bg_color))
+            painter.drawRoundedRect(
+                0, 0, self.width(), self.height(), self.height() / 2, self.height() / 2
+            )
+        elif self.isChecked():
+            painter.setBrush(QColor(self.active_color))
+            painter.drawRoundedRect(
+                0, 0, self.width(), self.height(), self.height() / 2, self.height() / 2
+            )
+
+    def hitButton(self, pos):
+        return self.contentsRect().contains(pos)
+
+    def mousePressEvent(self, event):
+        self.start_animation(not self.isChecked())
+
+if __name__ == "__main__":
+    from PyQt5.QtWidgets import QApplication, QVBoxLayout, QWidget
+
+    app = QApplication([])
+
+    window = QWidget()
+    window.setWindowTitle("Switch Control")
+    window.setGeometry(100, 100, 400, 200)
+
+    layout = QVBoxLayout()
+
+    switch = SwitchControl()
+    layout.addWidget(switch)
+
+    window.setLayout(layout)
+
+    window.show()
+
+    app.exec_()
