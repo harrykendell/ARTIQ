47c7e370e8531f325e820ee1b1c689c6bcabc91d
harrykendell <harry@kendell.co.uk>
2025-01-23 23:37:59 +0000

    just code well instead :)

--- repository/utils/surpress_missing_imports.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3
-from artiq.language.core import delay, now_mu, parallel, sequential
-from artiq.language.units import us, ms, MHz, dB, s
-from artiq.language.types import TInt32, TFloat, TList, TTuple, TStr, TNone
