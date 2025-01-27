00011131425ec02448efa1fcd585947dabdfef9e
harrykendell <harry@kendell.co.uk>
2025-01-27 19:03:19 +0000

    live imaging and track load rate

--- repository/gui/pco_camera.py
+++ repository/gui/pco_camera.py
@@ -4,7 +4,7 @@ Simple example of how to retrieve image data from pco.Camera.
 
 from artiq.experiment import *
 
-import logging
+import time
 import numpy as np
 import pco
 
@@ -14,9 +14,7 @@ from PyQt6.QtGui import QImage, QPixmap
 
 import pyqtgraph as pg
 
-logger = logging.getLogger()
-logger.setLevel(logging.WARNING)
-logger.addHandler(pco.stream_handler)
+# logger.addHandler(pco.stream_handler)
 
 triggers = [
     "auto sequence",  # just keeps imaging
@@ -24,6 +22,10 @@ triggers = [
     "external exposure start & software trigger",  # takes a picture when Trig goes high
     "external exposure control",  # seems to just always immediately take a picture?
 ]
+
+BINNING = 1
+FULL_ROI = (1, 1, 1392 // BINNING, 1040 // BINNING)
+WHOLE_CELL_ROI = (620, 475, 750, 650)
 """
 For the pixelfly:
     "serial": 19701804,
@@ -46,7 +48,8 @@ def init_cam(cam: pco.Camera):
     cam.configuration = {
         "timestamp": "binary",
         "trigger": triggers[0],
-        "exposure time": 0.1,
+        "exposure time": 100_00e-6,
+        "binning": (BINNING, BINNING),
     }
 
     print(f"{cam.camera_name} ({cam.camera_serial})")
@@ -59,14 +62,32 @@ class CameraWidget(QWidget):
         super().__init__()
         self.cam = cam
         self.initUI()
+
         self.start_recording()
+
+        self.first = True
 
     def initUI(self):
         # actual image
         pg.setConfigOptions(imageAxisOrder="row-major")
         self.layout = QVBoxLayout()
         self.im = pg.ImageView()
+
+        meanplot = pg.PlotWidget(axisItems={"bottom": pg.DateAxisItem()})
+        meanplot.setLabel("left", "Mean pixel value")
+        meanplot.setLabel("bottom", "Time")
+        meanplot.showGrid(x=True, y=True)
+        self.means = []
+        self.times = []
+        self.meancurve = meanplot.plot(
+            self.times,
+            self.means,
+            pen=pg.mkPen("y", width=2),
+        )
+
         self.layout.addWidget(self.im)
+        self.layout.addWidget(meanplot)
+
         self.setLayout(self.layout)
 
         self.timer = QTimer()
@@ -74,12 +95,35 @@ class CameraWidget(QWidget):
         self.timer.start(100)
 
     def update_image(self):
-        self.cam.wait_for_new_image()
-        self.im.setImage(self.cam.image()[0])
+        # essentially self.cam.wait_for_new_image() but for only fifo mode
+        settings = self.cam.rec.get_settings()
+        status = self.cam.rec.get_status()
+
+        # sequence mode
+        if settings["recorder type"]==0x0001:
+            self.cam.wait_for_new_image()
+
+        # hardware trigger fifo mode
+        if settings["recorder type"]==0x0003:
+            if status["dwProcImgCount"] == 0:
+                return
+        img,meta = self.cam.image(roi=(670, 540, 720, 580))
+        self.im.setImage(
+            img,
+            autoHistogramRange=self.first,
+            autoLevels=self.first,
+            autoRange=self.first,
+        )
+
+        self.times.append(time.time())
+        self.means.append(np.mean(img))
+        self.meancurve.setData(self.times, self.means)
+
+        self.first = False
 
     def start_recording(self):
-        self.cam.record(10, mode="fifo")
-
+        self.cam.record(10, mode='fifo')
+        # self.cam.record(10, mode="fifo")
 
 def main_gui():
     app = QApplication([])
@@ -94,6 +138,7 @@ def main_gui():
         widget.setGeometry(100, 100, 800, 600)
 
         app.exec()
+
 
 if __name__ == "__main__":
     main_gui()
