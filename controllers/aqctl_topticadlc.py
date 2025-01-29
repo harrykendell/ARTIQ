85ae0dfa2fab647078d4a67edfe2c25dd759cab9
harrykendell <harry@kendell.co.uk>
2025-01-29 23:13:29 +0000

    Exploring publishing laser state as well as the server loop

--- controllers/aqctl_topticadlc.py
+++ controllers/aqctl_topticadlc.py
@@ -5,8 +5,9 @@ import logging
 
 import sipyco.common_args as sca
 from sipyco.pc_rpc import simple_server_loop
-
+from sipyco.sync_struct import Notifier, Publisher
 from driver_topticadlc import TopticaDLCPro
+from toptica.lasersdk.client import Client, NetworkConnection, DeviceNotFoundError, Subscription, Timestamp, SubscriptionValue
 
 
 def get_argparser():
@@ -38,6 +39,45 @@ def main():
     dev.open()
     logging.info("Established connection.")
 
+
+    notifier = Notifier(
+        {
+            "emission-button-enabled": dev._dlcpro.emission_button_enabled.get(),
+            "emission": dev._dlcpro.emission.get(),
+
+            "laser1:enabled": dev._dlcpro.laser1.enabled.get(),
+            "laser1:dl:cc:current-set": dev._dlcpro.laser1.dl.cc.current_set.get(),
+            "laser1:amp:cc:current-set": dev._dlcpro.laser1.amp.cc.current_set.get(),
+            "laser1:dl:lock:lock-enabled": dev._dlcpro.laser1.dl.lock.lock_enabled.get(),
+
+            "laser2:enabled": dev._dlcpro.laser2.enabled.get(),
+            "laser2:dl:cc:current-set": dev._dlcpro.laser2.dl.cc.current_set.get(),
+            "laser2:amp:cc:current-set": dev._dlcpro.laser2.amp.cc.current_set.get(),
+            "laser2:dl:lock:lock-enabled": dev._dlcpro.laser2.dl.lock.lock_enabled.get(),
+        }
+    )
+    def callback(subscription: Subscription, _: Timestamp, value: SubscriptionValue):
+        notifier.set_value(subscription.name, value)
+
+    dev._dlcpro.emission_button_enabled.subscribe(callback)
+    dev._dlcpro.emission.subscribe(callback)
+
+    dev._dlcpro.laser1.enabled.subscribe(callback)
+    dev._dlcpro.laser1.dl.cc.current_set.subscribe(callback)
+    dev._dlcpro.laser1.amp.cc.current_set.subscribe(callback)
+    dev._dlcpro.laser1.dl.lock.lock_enabled.subscribe(callback)
+
+    dev._dlcpro.laser2.enabled.subscribe(callback)
+    dev._dlcpro.laser2.dl.cc.current_set.subscribe(callback)
+    dev._dlcpro.laser2.amp.cc.current_set.subscribe(callback)
+    dev._dlcpro.laser2.dl.lock.lock_enabled.subscribe(callback)
+                 
+    publisher = Publisher(notifiers={"State": notifier})
+    publisher.start(
+        host=sca.bind_address_from_args(args),
+        port=args.port - 1,
+    )
+
     try:
         logging.info("Starting server at port {}...".format(args.port))
         simple_server_loop(
@@ -46,6 +86,7 @@ def main():
             args.port,
         )
     finally:
+        publisher.stop()
         dev.close()
 
 
