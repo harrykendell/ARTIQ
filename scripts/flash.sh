976e6c79940e3e01db46f8c4489b642c6aede042
harrykendell <harry@kendell.co.uk>
2025-02-09 14:50:03 +0000

    keep tool path flexible since it updates on new releases

--- scripts/flash.sh
+++ scripts/flash.sh
@@ -12,12 +12,14 @@ cd $SCRIPT_DIR/..
 
 echo $(pwd)
 # check for required build tool
-if [[ -d /opt/Xilinx/Vivado/2022.2 ]]; then
-    echo -e "${GREEN}Vivado 2020.2 found, flashing artiq crate${NC}"
+TOOL_PATH="$(grep 'set -e; source /opt/Xilinx/Vivado' src/artiq/flake.nix | grep -Po '/opt/Xilinx/Vivado/[0-9]+\.[0-9]+')"
+
+if [[ -d $TOOL_PATH ]]; then
+    echo -e "${GREEN}${TOOL_PATH} found, flashing artiq crate${NC}"
 
     # ensure the reference device_db reflects the currently flashed details
     nix develop git+https://github.com/m-labs/artiq.git/?ref=release-8#boards --command bash -c "artiq_ddb_template crate\ config/bu2402001.json > crate\ config/device_db.py"
-    
+
     # build firmware - 8.0+
     nix develop git+https://github.com/m-labs/artiq.git/?ref=release-8#boards --command bash -c "python -m artiq.gateware.targets.kasli crate\ config/bu2402001.json"
 
@@ -27,5 +29,6 @@ if [[ -d /opt/Xilinx/Vivado/2022.2 ]]; then
 
     echo -e "${GREEN}Flashing complete, firmware updated in 'crate config/bu2402001_current'${NC}"
 else
-    echo -e "${RED}Vivado 2020.2 not found, cannot build and flash artiq crate${NC}"
+    echo -e "${RED}${TOOL_PATH} not found, cannot build and flash artiq crate${NC}"
+    echo -e "Available versions:\n$(ls /opt/Xilinx/Vivado)"
 fi
