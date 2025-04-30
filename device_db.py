55b00a4e99e993e20f7c587c19dd5d2504d15528
harrykendell <harry@kendell.co.uk>
2025-04-30 18:59:14 +0100

    spelling

--- device_db.py
+++ device_db.py
@@ -324,7 +324,7 @@ aliases = {
     # photodiode on 3DMOT - given we don't enable servoing for the locking arm
     "MOT_photodiode": "suservo_ch0",
     # 780 Laser
-    "780_unlock": "ttl 4",
+    "780_unlock": "ttl4",
     "suservo_aom_LOCK": "suservo_ch0",
     "suservo_aom_MOT": "suservo_ch1",  # has its photodiode on it's channel
     "suservo_aom_IMG": "suservo_ch2",
